0c62b9b66d663b845863afc8ab631f7f5f1e85fa
Philip Hansen <me@philiphansen.dk>
2015-12-08 17:13:25 +0100

    Added function to parse input The user input can now be parsed using a function, which will seperate the input into three variables and return them.

--- Diceroller.py
+++ Diceroller.py
@@ -1,4 +1,24 @@
 import random
+import re
+
+#Get variables from input string
+def read_input_string(input_string):
+    if not "d" in input_string.lower():
+        raise ValueError("Input must be in the format [rolls]d[sides]+/-[sum to add]")
+
+    number_of_rolls, rest = input_string.lower().split("d")
+    number_of_rolls = int(number_of_rolls)
+
+    operator_index = re.search("\+|-", rest)
+
+    if operator_index:
+        add_sum = int(rest[operator_index.start():])
+        dice_size = int(rest[:operator_index.start()])
+    else:
+        add_sum = None
+        dice_size = int(rest)
+
+    return number_of_rolls, dice_size, add_sum
 
 #Dice function
 def roll(dice_sides, rolls):
@@ -9,3 +29,7 @@ def roll(dice_sides, rolls):
 
 #Debugging print
 print(roll(4, 3))
+
+input_string = raw_input("Dice roll: ")
+
+print read_input_string(input_string)
