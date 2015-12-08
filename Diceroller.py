04ea9890736e5b86013450273ac4e15edec0d399
LookACastle <thorkik@gmail.com>
2015-12-08 16:29:44 +0100

    Edited for clarity
    Edited variable names to clarify the meaning of the variables, and to follow pep standards by using lowercase.

--- Diceroller.py
+++ Diceroller.py
@@ -1,8 +1,11 @@
 import random
-def roll(dice, rools):
-    dSum = 0
-    for x in range(rools):
-        dSum += random.randint(1, dice)
-    return(dSum)
 
+#Dice function
+def roll(dice_sides, rolls):
+    dice_sum = 0
+    for x in range(rolls):
+        dice_sum += random.randint(1, dice_sides)
+    return(dice_sum)
+
+#Debugging print
 print(roll(4, 3))
