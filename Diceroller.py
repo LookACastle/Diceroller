ebcd532fd87291b0251e7356e188217824a4403e
LookACastle <thorkik@gmail.com>
2015-12-10 09:55:01 +0100

    Made it an option whether or not you want individual rolls

--- Diceroller.py
+++ Diceroller.py
@@ -7,8 +7,16 @@ def read_input_string(input_string):
         raise ValueError("Input must be in the format [rolls]d[sides]+/-[Modifiers]")
 
     number_of_rolls, rest = input_string.lower().split("d")
-    number_of_rolls = int(number_of_rolls)
 
+    #Turn individual rolls on or off
+    if "i" in rest:
+        rest, individual_rolls = rest.split("i")
+        individual_rolls = True
+    else:
+        individual_rolls = False
+
+
+    #Adding weapon support
     if "w" in rest:
         rest, weapon_id = rest.split("w")
     else:
@@ -23,15 +31,20 @@ def read_input_string(input_string):
         add_sum = None
         dice_size = int(rest)
 
-    return number_of_rolls, dice_size, add_sum, weapon_id
+    number_of_rolls = int(number_of_rolls)
+
+    return number_of_rolls, dice_size, add_sum, weapon_id, individual_rolls
 
 #Dice function
-def roll(dice_sides, rolls):
+def roll(dice_sides, rolls, individual_rolls):
     dice_sum = 0
     for x in range(rolls):
         curr_roll = random.randint(1, dice_sides)
         dice_sum += curr_roll
-        print "you rolled a:", curr_roll, "on roll no.", x+1
+        if individual_rolls:
+            print "you rolled a:", curr_roll, "on roll no.", x+1
+        else:
+            pass
 
         #Crits and fumbles
         if dice_sides == 20 and dice_sum == 1 and rolls == 1:
@@ -40,18 +53,14 @@ def roll(dice_sides, rolls):
             dice_sum = "a crit!"
     return(dice_sum)
 
-#Debugging print
-"""
-print(roll(4, 3))
-"""
 
 #Runs the functions
 while True:
     input_string = raw_input("Dice roll: ")
 
-    number_of_rolls, dice_size, add_sum, weapon_id = read_input_string(input_string)
+    number_of_rolls, dice_size, add_sum, weapon_id, individual_rolls = read_input_string(input_string)
 
-    sum = roll(dice_size, number_of_rolls)
+    sum = roll(dice_size, number_of_rolls, individual_rolls)
     if sum not in ["a crit!", "a fumble!"] and add_sum:
         sum += add_sum
 
